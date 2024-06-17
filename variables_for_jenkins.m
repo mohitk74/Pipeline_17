b38812b9663e0b406164ff8067f3fd0fd270eb86
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-06-17 10:42:08 +0530

    Update variables_for_jenkins.m

--- variables_for_jenkins.m
+++ variables_for_jenkins.m
@@ -1,20 +1,29 @@
 clc;clear all;
-% var_type = input("Enter 1 or 2 based on the type of data used : \n1 -> boolean\n2 -> double\n")
+% Read the table from Excel sheet & store the table in the variable named 'data'
 data=readtable('Wiper_washer_inputs.xlsx');
+%-----------------------------------------------------------------------------------------------------
+% Get the time data from the 1st column of the table.
+% NOTE : Time data must be the first column of the excel sheet & must be written in 'Time' format only
 time=seconds(data.Time(2:end));
+%-----------------------------------------------------------------------------------------------------
 for i = 2:width(data)
-    % Extract variable
+    % Extract data of each variable from excel sheet.
     var = data{2:end,i};
-    % Create timetable and load it to the workspace
+    % var_type is a variable that stores the values 197,199 etc.
+    % NOTE : 197 must be only used for Boolean values.
+    % NOTE : 199 must be only used for double values.
+    % NOTE : The 2nd row of the excel sheet must be having the above values to classify the data type.
     var_type = data{1,i};
-    % var_type
     if var_type == 197
         var = logical(var);
     else
         var = double(var);
     end
+    % To read the variable names & store them in varName.
     varName = data.Properties.VariableNames{i};
+    % Create timetable and load it to the workspace
     T = timetable(time,var);
     assignin('base',varName,T);
 end
+%-----------------------------------------------------------------------------------------------------
 save("acd.mat")
